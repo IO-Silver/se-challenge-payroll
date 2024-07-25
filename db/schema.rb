282a1ded37e12898c4dc699ab0e6a3a4ddbb9890
Rio Kivell <rjckivel@gmail.com>
2024-07-25 15:56:45 -0400

    Created JobGroup resource

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema[7.1].define(version: 2024_07_25_194902) do
   create_table "job_groups", force: :cascade do |t|
     t.integer "rate"
+    t.string "name"
+    t.string "primary_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
