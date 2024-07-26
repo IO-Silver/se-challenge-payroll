16ed60f884390bde5e1d95151fb4d6a39c89df5f
Rio Kivell <rjckivel@gmail.com>
2024-07-26 12:57:34 -0400

    Now saving pay periods on RoW creation.  Taking a break for now

--- app/models/record_of_work.rb
+++ app/models/record_of_work.rb
@@ -1,9 +1,17 @@
 class RecordOfWork < ApplicationRecord
   belongs_to :report
   belongs_to :employee
-  after_save :set_pay_period
+  after_create :set_pay_period
 
   def set_pay_period
+    if date <= date.beginning_of_month + 14.days
+      self.pay_period_start = date.beginning_of_month
+      self.pay_period_end   = date.beginning_of_month + 14.days
+    else
+      self.pay_period_start = date.beginning_of_month + 15.days
+      self.pay_period_end   = date.end_of_month
+    end
 
+    save!
   end
 end
